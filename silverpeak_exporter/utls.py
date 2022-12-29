ffd47b881caa4c68de56c09eb2d2e15418b19826
IP Headers <110433751+ipHeaders@users.noreply.github.com>
2022-12-29 14:07:33 -0500

    updating GetAppVersion function

--- silverpeak_exporter/utls.py
+++ silverpeak_exporter/utls.py
@@ -67,8 +67,8 @@ def getApplianceID(name,applianceDict:dict):
 
 # Reads the toml file to find out application version
 def getAppVersion():
-    pathDir = os.path.dirname(os.getcwd())
-    tomlFile = os.path.join(pathDir,'silverpeak-prometheus/pyproject.toml')
+    pathDir = os.path.dirname(os.path.realpath("pyproject.toml"))
+    tomlFile = os.path.join(pathDir,'pyproject.toml')
     with open(tomlFile, "rb") as f:
         data = tomli.load(f)
         return data['tool']['poetry']['version']
